3ef9c314708c50b660cefbecea0dfbff12ca0909
BigSwap Finance <86725876+BigSwap-Finance@users.noreply.github.com>
2022-03-15 17:13:25 -0400

    Update fetchVaultUser.ts

--- src/state/pools/fetchVaultUser.ts
+++ src/state/pools/fetchVaultUser.ts
@@ -11,7 +11,7 @@ const fetchVaultUser = async (account: string) => {
       userShares: new BigNumber(userContractResponse.shares.toString()).toJSON(),
       lastDepositedTime: userContractResponse.lastDepositedTime.toString(),
       lastUserActionTime: userContractResponse.lastUserActionTime.toString(),
-      cakeAtLastUserAction: new BigNumber(userContractResponse.bgspAtLastUserAction.toString()).toJSON(),
+      cakeAtLastUserAction: new BigNumber(userContractResponse.GolAtLastUserAction.toString()).toJSON(),
     }
   } catch (error) {
     return {
